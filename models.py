af096629186aac0d6f8124021087c292a5e4da7b
Ojaswi Sinha <os675524@ohio.edu>
2025-05-30 05:09:03 -0400

    New Data MNIST

--- models.py
+++ models.py
@@ -1,127 +1,68 @@
-# import tensorflow as tf
-# from tensorflow.keras import layers, models
-# import numpy as np
-
-# class MNISTModel:
-#     def __init__(self, input_shape=(28, 28), num_classes=10):
-#         self.model = models.Sequential([
-#             layers.Flatten(input_shape=input_shape),
-#             layers.Dense(100, activation='relu'),
-#             layers.Dense(100, activation='relu'),
-#             layers.Dense(num_classes)
-#         ])
-
-#     def get_params(self):
-#         return self.model.get_weights()
-
-#     def set_params(self, weights):
-#         self.model.set_weights(weights)
-
-#     @property
-#     def trainable_weights(self):
-#         return self.model.trainable_weights
-#     @property
-#     def trainable_variables(self):
-#         return self.model.trainable_variables
-
-#     def __call__(self, x, training=False):
-#         return self.model(x, training=training)
-
-# class CIFARModel:
-#     def __init__(self, input_shape=(32, 32, 3), num_classes=10):
-#         self.model = models.Sequential([
-#             layers.Conv2D(16, (3, 3), activation='relu', input_shape=input_shape),
-#             layers.MaxPooling2D(pool_size=(2, 2)),
-#             layers.Conv2D(32, (3, 3), activation='relu'),
-#             layers.MaxPooling2D(pool_size=(2, 2)),
-#             layers.Flatten(),
-#             layers.Dense(100, activation='relu'),
-#             layers.Dense(num_classes)
-#         ])
-
-#     def get_params(self):
-#         return self.model.get_weights()
-
-#     def set_params(self, weights):
-#         self.model.set_weights(weights)
-
-#     @property
-#     def trainable_weights(self):
-#         return self.model.trainable_weights
-#     @property
-#     def trainable_variables(self):
-#         return self.model.trainable_variables  
-
-#     def __call__(self, x, training=False):
-#         return self.model(x, training=training)
-
-
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, models
+import numpy as np
 
-class BaseModel:
-    def __init__(self):
-        self.model = None
+class MNISTModel:
+    def __init__(self, input_shape=(28, 28), num_classes=10):
+        self.model = models.Sequential([
+            layers.Flatten(input_shape=input_shape),
+            layers.Dense(100, activation='relu'),
+            layers.Dense(100, activation='relu'),
+            layers.Dense(num_classes)
+        ])
 
-    def build_model(self, input_shape, num_classes):
-        raise NotImplementedError
+    def get_params(self):
+        return self.model.get_weights()
 
-    def train(self, train_loader, epochs=1, lr=0.01):
-        optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
-        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    def set_params(self, weights):
+        self.model.set_weights(weights)
 
-        for epoch in range(epochs):
-            for x, y in train_loader:
-                if len(x.shape) == 3:
-                    x = tf.expand_dims(x, axis=0)
-                if x.shape[1] == 3 and x.shape[-1] != 3:
-                    x = tf.transpose(x, perm=[0, 2, 3, 1])
-                y = tf.convert_to_tensor(y)
-                y = tf.reshape(y, [-1])
-                with tf.GradientTape() as tape:
-                    logits = self.model(x, training=True)
-                    loss = loss_fn(y, logits)
-                grads = tape.gradient(loss, self.model.trainable_variables)
-                optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+    @property
+    def trainable_weights(self):
+        return self.model.trainable_weights
+    @property
+    def trainable_variables(self):
+        return self.model.trainable_variables
+    
+    def get_weights(self):
+        return self.model.get_weights()
 
-    def evaluate(self, test_loader):
-        acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
-        for x, y in test_loader:
-            if len(x.shape) == 3:
-                x = tf.expand_dims(x, axis=0)
-            if x.shape[1] == 3 and x.shape[-1] != 3:
-                x = tf.transpose(x, perm=[0, 2, 3, 1])
-            y = tf.convert_to_tensor(y)
-            y = tf.reshape(y, [-1])
-            logits = self.model(x, training=False)
-            acc_metric.update_state(y, logits)
-        return acc_metric.result().numpy()
+    def set_weights(self, weights):
+        self.model.set_weights(weights)
 
+    def __call__(self, x, training=False):
+        return self.model(x, training=training)
 
-class MNISTModel(BaseModel):
-    def __init__(self, input_shape=(28, 28, 1), num_classes=10):
-        super().__init__()
-        self.model = keras.Sequential([
-            keras.Input(shape=input_shape),
-            layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
+class CIFARModel:
+    def __init__(self, input_shape=(32, 32, 3), num_classes=10):
+        self.model = models.Sequential([
+            layers.Conv2D(16, (3, 3), activation='relu', input_shape=input_shape),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(32, (3, 3), activation='relu'),
             layers.MaxPooling2D(pool_size=(2, 2)),
             layers.Flatten(),
-            layers.Dense(128, activation='relu'),
+            layers.Dense(100, activation='relu'),
             layers.Dense(num_classes)
         ])
 
+    def get_params(self):
+        return self.model.get_weights()
 
-class CIFARModel(BaseModel):
-    def __init__(self, input_shape=(32, 32, 3), num_classes=10):
-        super().__init__()
-        self.model = keras.Sequential([
-            keras.Input(shape=input_shape),
-            layers.Conv2D(32, (3, 3), activation='relu'),
-            layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(64, (3, 3), activation='relu'),
-            layers.MaxPooling2D((2, 2)),
-            layers.Flatten(),
-            layers.Dense(64, activation='relu'),
-            layers.Dense(num_classes)
-        ])
+    def set_params(self, weights):
+        self.model.set_weights(weights)
+
+    @property
+    def trainable_weights(self):
+        return self.model.trainable_weights
+    @property
+    def trainable_variables(self):
+        return self.model.trainable_variables  
+    
+    def get_weights(self):
+        return self.model.get_weights()
+
+    def set_weights(self, weights):
+        self.model.set_weights(weights)
+
+    def __call__(self, x, training=False):
+        return self.model(x, training=training)
--- run.sh
+++ run.sh
@@ -2,63 +2,16 @@
 
 echo "🔁 Starting Basil Experiments..."
 
-# Base config
-# datasets=("m" "c")
-# attacks=("none" "gaussian" "sign_flip" "hidden")
-# nodes=10
-# S=10
-# rounds=20
-# epochs=50
-# iid="y"
-# acds="n"
-# basilPlus="n"
-# attkID="2,5,7"
-
-# for dataset in "${datasets[@]}"; do
-#     for attack in "${attacks[@]}"; do
-#         echo "======================= CONFIG ================="
-#         echo "Dataset               : $dataset"
-#         echo "Nodes                 : $nodes"
-#         echo "S                     : $S"
-#         echo "Rounds                : $rounds"
-#         echo "Epochs                : $epochs"
-#         echo "IID [y/n]             : $iid"
-#         echo "ACDS [y/n]            : $acds"
-#         echo "Basil Plus [y/n]      : $basilPlus"
-#         echo "Attack                : $attack"
-#         echo "Attack IDs (Nodes)    : $attkID"
-#         echo "================================================="
-
-#         python3 main_basil.py <<EOF
-# $dataset
-# $nodes
-# $S
-# $rounds
-# $epochs
-# $iid
-# $acds
-# $basilPlus
-# $attack
-# $attkID
-# EOF
-#     done
-# done
-
-
-
-
-#Testing configuration for CIFAR-10
-
-
-dataset='c'
+dataset="m"
 nodes=10
 S=10
-rounds=5
+rounds=35
 epochs=1
+iid="y"
+acds="n"
+basilPlus="n"
 
-iid='y'
-acds='n'
-basilPlus='n'
+echo ""
 echo "======================= CONFIG ================="
 echo "Dataset               : $dataset"
 echo "Nodes                 : $nodes"
@@ -70,6 +23,7 @@ echo "ACDS [y/n]            : $acds"
 echo "Basil Plus [y/n]      : $basilPlus"
 echo "================================================="
 
+
 python3 main_basil.py <<EOF
 $dataset
 $nodes
@@ -77,6 +31,8 @@ $S
 $rounds
 $epochs
 $iid
+$acds
 $basilPlus
 EOF
-echo "✅ All experiments completed."
+
+echo "All experiments completed."
